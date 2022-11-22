be4c10f82380cbbcfdf4a10c76da52cdf6b66b80
mohmadashik <ashikg8coder@gmail.com>
2022-11-22 18:52:48 +0530

    first review modifications added

--- app.py
+++ app.py
@@ -3,16 +3,17 @@ from flask_jwt_extended import (
     JWTManager,
     jwt_required,
     create_access_token,
-    get_jwt
+    get_jwt,
+    get_jwt_identity                 #to get the email id of user from current jwt token
     )
-import pdb
 import redis
 import re
 from datetime import timedelta,datetime
+from bson.objectid import ObjectId
 
 from pymongo import MongoClient
 
-ACCESS_EXPIRES = timedelta(hours=4)
+ACCESS_EXPIRES = timedelta(hours=10)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] ='thisissecretkey'
@@ -26,8 +27,6 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client['moviesdb']
 user_col = db['user']
 movie_col = db['movie']
-interest_col = db['user_interest']
-
 
 jwt_redis_blocklist = redis.StrictRedis(
     host="localhost", port=6379, db=0, decode_responses=True
@@ -39,10 +38,14 @@ def check_if_token_is_revoked(jwt_header, jwt_payload: dict):
     token_in_redis = jwt_redis_blocklist.get(jti)
     return token_in_redis is not None
 
-@app.route('/register',methods=['GET','POST'])
+@app.route("/")
+def home():
+    return jsonify({"Status":'the site is active'}),200
+
+@app.route('/register',methods=['POST'])
 def register():
     status = ''
-    if request.method == 'POST' and 'username' in request.json and 'password' in request.json and 'email' in request.json :
+    if 'username' and 'password' and 'email' in request.json :
         username = request.json['username']
         password = request.json['password']
         email = request.json['email']
@@ -64,17 +67,16 @@ def register():
         status = 'Please fill out the form !'
     return jsonify({"status": status})
 
-@app.route('/login', methods=['POST'])
+@app.route('/login')
 def login():
-    # pdb.set_trace()
-    if request.method=="POST" and 'username' in request.json and 'password' in request.json:
-        username = request.json['username']
+    if 'email' in request.json and 'password' in request.json:
+        email = request.json['email']
         password = request.json['password']
-        check_user = user_col.find_one({'username':username})
+        check_user = user_col.find_one({'email':email})
         if check_user and check_user['password']==password:
             output = {
                 'Status'    : 'access token created',
-                'access_token': create_access_token(identity=username)
+                'access_token': create_access_token(identity=email)
             }
             return jsonify(output), 200
         else:
@@ -82,11 +84,10 @@ def login():
             return jsonify(output),400
     return make_response('could not verify',  401, {'Authentication': '"login required"'})
 
-@app.route('/profile',methods=['GET'])
+@app.route('/profile')
 @jwt_required()
 def profile():
     return jsonify({'Status':'profile page is active'})
-
 
 # Endpoint for revoking the current users access token
 @app.route('/logout', methods=['DELETE'])
@@ -94,164 +95,76 @@ def profile():
 def logout():
     jti = get_jwt()["jti"]
     jwt_redis_blocklist.set(jti, "", ex=ACCESS_EXPIRES)
-    return jsonify(status="Access token revoked")
-
-
-@app.route("/")
-def home():
-    return jsonify({"Status":'the site is active'}),200
+    return jsonify({'status':'Access token revoked'})
 
 @app.route('/addmovie',methods=['POST'])
 @jwt_required()
 def addmovie():
-    status=''
-    if request.method == 'POST' and 'name' in request.json and 'releasedate' in request.json and 'genre' in request.json:
-        name = request.json['name']
-        releasedate = request.json['releasedate']
+    if 'movie' and 'date' and 'genre' in request.json:
+        movie = request.json['movie']
+        date  = request.json['date']
         genre = request.json['genre']
-        review = request.json['review']
-        check_movie = movie_col.find_one({'name':name})
-        # pdb.set_trace()
-        if check_movie!=None and check_movie['name']==name:
-            status = "movie already exists"
-        elif not name or not releasedate or not genre:
-            status = "please fill out the form"
-        else:
-            movie_col.insert_one({'name':name,'releasedate':releasedate,'genre':genre,'upvotes':0,'downvotes':0,'review':review})
-            status = "movie added successfully"
-    elif request.method =="POST":
-        status = "please fill out the form"
-    return jsonify({"status":status})
+        check_movie = movie_col.find_one({'movie':movie})
+        inserted_id = movie_col.insert_one({'movie':movie,'date':date,'genre':genre,'upvotes':0,'downvotes':0 }).inserted_id
+        output = {
+            'status': "movie added successfully",
+            'inserted_id' : str(inserted_id)
+          }
+    else:
+        output = {'status' : "please fill out the form"}
+    return jsonify(output)
 
-@app.route('/viewmovies',methods=['GET'])
-# @jwt_required()
+@app.route('/viewmovies')
 def movielist():
     documents = movie_col.find()
     output = [{item: data[item] for item in data if item!='_id'} for data in documents]
     return jsonify(output)
 
-@app.route('/editmovie',methods=['PUT'])
+@app.route('/editmovie/<movieid>/',methods=['PUT'])
 @jwt_required()
-def editmovie():
-    data = request.json
-    filt = data['Filter']
-    updated_data = {"$set":data['DataToBeUpdated']}
+def editmovie(movieid):
+    filt = {'_id':ObjectId(movieid)}
+    updated_data = {"$set":request.json}
     response = movie_col.update_one(filt,updated_data)
-    output = {'Status':'Succesfully Updated' if response.modified_count>0 else"Nothing was updated."}
+    output = {'Status':'Succesfully Updated' if response.modified_count>0 else "Nothing was updated."}
     return output
 
-@app.route('/deletemovie',methods=['DELETE'])
+@app.route('/deletemovie/<movieid>/',methods=['DELETE'])
 @jwt_required()
-def deletemovie():
-    filt = request.json['Filter']
-    response = movie_col.delete_one(filt)
+def deletemovie(movieid):
+    response = movie_col.delete_one({"_id":ObjectId(movieid)})
     output = {'Status': 'Successfully Deleted' if response.deleted_count > 0 else "Document not found."}
     return output
 
 @app.route('/setgenre',methods=['PUT'])
 @jwt_required()
 def setgenre():
-    data = request.json
-    filt = request.json['Filter']
-    updated_data = {"$set":data['DataToBeUpdated']}
+    filt = {"email":get_jwt_identity()}
+    updated_data = {"$set":request.json}
     response = user_col.update_one(filt,updated_data)
     output = {'Status':'Succesfully Updated The Genre' if response.modified_count>0 else"Nothing was updated."}
     return output
-@app.route('/voting',methods=['PUT'])
-@jwt_required()
-def voting():
-    # pdb.set_trace()
-    data = request.json
-    username = data['username']
-    moviename = data['name']
-    vote = data['vote']
-    check_user = interest_col.find_one({'username':username})
-    if check_user!=None:
-        old_vote = check_user['votes'][moviename] if moviename in check_user['votes'] else None
-        is_old_movie = True if moviename in check_user['votes'] else False
-        if is_old_movie and vote == old_vote:
-            status = "you have given the same vote already"
-        else:
-            if vote==1:
-                if is_old_movie:
-                    new_upvotes = movie_col.find_one({'name':moviename})['upvotes']+1
-                    new_downvotes = movie_col.find_one({'name':moviename})['downvotes']-1
-                else:
-                    new_upvotes =  movie_col.find_one({'name':moviename})['upvotes']+1
-                    new_downvotes = movie_col.find_one({'name':moviename})['downvotes']
-                interest_query = {'votes.'+moviename:1}
-                interest_update = {"$set":interest_query}
-                response1 = interest_col.update_one({'username':username},interest_update)
-                movie_query ={
-                'upvotes':new_upvotes,
-                'downvotes':new_downvotes
-                }
-                movie_update = {"$set":movie_query}
-                response2 = movie_col.update_one({"name":moviename},movie_update)
-                status = "upvoted successfully"
-            else:
-                if is_old_movie:
-                    new_upvotes = movie_col.find_one({'name':moviename})['upvotes']-1
-                    new_downvotes = movie_col.find_one({'name':moviename})['downvotes']+1
-                else:
-                    new_upvotes = movie_col.find_one({'name':moviename})['upvotes']
-                    new_downvotes = movie_col.find_one({'name':moviename})['downvotes']+1
-                interest_query = {'votes.'+moviename:-1}
-                interest_update = {"$set":interest_query}
-                response1 = interest_col.update_one({'username':username},interest_update)
-                movie_query ={
-                'upvotes':new_upvotes,
-                'downvotes':new_downvotes
-                }
-                movie_update = {"$set":movie_query}
-                response2 = movie_col.update_one({"name":moviename},movie_update)
-                status = "downvoted successfully"
-    else:
-        interest_query = {'username':username,'votes':{moviename:0}}
-        interest_col.insert_one(interest_query)
-        if vote==1:
-            new_upvotes = movie_col.find_one({'name':moviename})['upvotes']+1
-            interest_query = {'votes.'+moviename:1}
-            interest_update = {"$set":interest_query}
-            response1 = interest_col.update_one({'username':username},interest_update)
-            movie_query ={
-            'upvotes':new_upvotes
-            }
-            movie_update = {"$set":movie_query}
-            response2 = movie_col.update_one({"name":moviename},movie_update)
-            status = "upvoted successfully"
-        else:
-            new_downvotes = movie_col.find_one({'name':moviename})['downvotes']+1
-            interest_query = {'votes.'+moviename:-1}
-            interest_update = {"$set":interest_query}
-            response1 = interest_col.update_one({'username':username},interest_update)
-            movie_query ={
-            'downvotes':new_downvotes
-            }
-            movie_update = {"$set":movie_query}
-            response2 = movie_col.update_one({"name":moviename},movie_update)
-            status = "downvoted successfully"
-    return {'status':status}
 
-@app.route('/recommendations',methods=['GET'])
+@app.route('/recommendations')
 @jwt_required()
 def getrecommendations():
-    username = request.json['username']
-    fav_genre = user_col.find_one({"username":username})['genre']
+    email = get_jwt_identity()
+    fav_genre = user_col.find_one({"email":email})['genre']
     documents = movie_col.find({"genre":fav_genre})
     output = [{item: data[item] for item in data if item!='_id'} for data in documents]
     return jsonify(output)
 
-@app.route('/listmovies')
+@app.route('/sortmovies')
 def sortmovies():
     status = ''
     sorting_key = request.json['sorting_key']
-    if sorting_key =='releasedate':
+    order = 'desc' if 'order' not in request.json else request.json['order']
+    if sorting_key =='date':
         movies = movie_col.find()
         movies = list(movies)
         for movie in movies:
-            movie['releasedate'] = datetime.strptime(movie['releasedate'],"%d-%m-%Y").date()
-        sorted_movies=sorted(movies,key=lambda i:i['releasedate'])
+            movie['date'] = datetime.strptime(movie['date'],"%d-%m-%Y").date()
+        sorted_movies=sorted(movies,key=lambda i:i['date'])
         output = [{item:data[item]for item in data if item!='_id'}for data in sorted_movies]
     elif sorting_key=='upvotes':
         sorted_movies = movie_col.find().sort("upvotes",-1)
@@ -261,8 +174,40 @@ def sortmovies():
         status = "you must give a valid sorting key"
         return jsonify({"status":status})
     output = [{item: data[item] for item in data if item!='_id'} for data in sorted_movies]
+    output = output if order=="desc" else output[-1::-1]
     return jsonify(output)
 
+@app.route('/voting/<movieid>/',methods=['PUT'])
+@jwt_required()
+def voting(movieid):
+    output = { 'status':'nothing happened' }
+    new_vote = request.json['vote']
+    current_user_email = get_jwt_identity()
+    current_user_id   = user_col.find_one({"email":current_user_email})['_id']
+    movieid = ObjectId(movieid)
+    current_movie = movie_col.find_one(movieid)
+    old_upvotes,old_downvotes = current_movie['upvotes'],current_movie['downvotes']
+    new_upvotes = old_upvotes
+    new_downvotes = old_downvotes
+    if not str(current_user_id) in current_movie:
+        if new_vote ==1:
+            new_upvotes+=1
+        else:
+            new_downvotes+=1
+    else:
+        user_old_vote = current_movie[str(current_user_id)]
+        if user_old_vote != new_vote:
+            if new_vote ==1:
+                new_upvotes +=1
+                new_downvotes-=1
+            else:
+                new_downvotes +=1
+                new_upvotes -=1
+    filt = {"_id":movieid}
+    updated_data = {"$set":{str(current_user_id):new_vote,"upvotes":new_upvotes,"downvotes":new_downvotes}}
+    response = movie_col.update_one(filt,updated_data)
+    output = { "status":"voted successfully"}
+    return jsonify(output)
 
 if __name__=='__main__':
     # pdb.set_trace()
